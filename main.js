5ed4ba9fc6f39de8cc056030a432bbfd9e6813a4
woodo8 <80512749+woodo8@users.noreply.github.com>
2021-03-13 16:08:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function skills() {
+	document.getElementById("upper1").style.animationName = "html";
+	document.getElementById("upper1").style.width = "90%";
+	document.getElementById("upper2").style.animationName = "css";
+	document.getElementById("upper2").style.width = "90%";
+	document.getElementById("upper3").style.animationName = "sass";
+	document.getElementById("upper3").style.width = "90%";
+	document.getElementById("upper4").style.animationName = "bstrap";
+	document.getElementById("upper4").style.width = "90%";
+
+	document.getElementById("upper5").style.animationName = "javascript";
+	document.getElementById("upper5").style.width = "90%";
+	document.getElementById("upper6").style.animationName = "python";
+	document.getElementById("upper6").style.width = "90%";
+	document.getElementById("upper7").style.animationName = "react";
+	document.getElementById("upper7").style.width = "90%";
+	document.getElementById("upper8").style.animationName = "linux";
+	document.getElementById("upper8").style.width = "90%";
+}
